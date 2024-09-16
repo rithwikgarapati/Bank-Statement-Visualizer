62a205e1e5b52809f82f9f7611e607d1bed2518e
Rithwik Garapati <garapatr@uci.edu>
2024-09-15 17:03:58 -0700

    Extracted city, state, and main description terms

--- Get_Data/Extract_data.py
+++ Get_Data/Extract_data.py
@@ -1,4 +1,33 @@
 import pandas as pd
+import re
+
+def process_description(description: str):
+    """
+        - This function extracts 
+            1. City
+            2. State
+            3. Main keywords of description
+        
+        - Returns (city, state, cleaned_desc)
+    """
+    description = description.split(' ')[4 : ]
+    description = [item.strip() for item in description if item.strip()]
+    
+    city = description[-2]
+    state = description[-1]
+    
+    if(len(state) > 2):
+        city += state[:-2]
+        state = state[-2:]
+    
+    description = (' '.join(description[ : -2]))
+
+    # Remove un-necessary prefixes.
+    cleaned = re.sub(r'^(SQ \*|BYT\*|TST\*|POS|CARD|ATM)', '', description)
+    
+    return (city, state, cleaned)
+    
+    
 
 def read_csv( file_name ):
     """
@@ -13,13 +42,16 @@ def read_csv( file_name ):
     for row in df.itertuples():
         description = row.Extended_Description
         amount = row.Amount
-
-        description = description.split(' ')[4 : ]
-        description = ' '.join(description)
-
+        
+        process_description(description)
+        city, state, main_desc = process_description(description)
+        
         trans = {
-            'description' : description,
+            'description' : main_desc,
             'amount' : amount,
+            'city' : city,
+            'state' : state,
+            'type' : 'spending' if amount < 0 else 'earning'
         }
 
         transactions.append(trans)
@@ -28,6 +60,7 @@ def read_csv( file_name ):
 
 
 def main():
+    read_csv('Get_Data/credit_card_transactions.csv')
     transactions = read_csv('Get_Data/credit_card_transactions.csv')
 
     for trans in transactions:
